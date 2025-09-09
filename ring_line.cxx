a1dde5ec671e6fa4f24d092ae2504b473df7b849
UlyanaGru <giperbolicheskiisinys@gmail.com>
2025-09-09 21:52:13 +0300

    convert time

--- ring_line.cxx
+++ ring_line.cxx
@@ -5,3 +5,13 @@
 #include <iomanip>
 #include <sstream>
 #include <map>
+
+using namespace std;
+
+//Преобразование времени 'hh:mm' в минуты, начиная с 00:00
+int timeToMinutes(const string& timeStr) {
+    size_t colonPos = timeStr.find(':');
+    int hours = stoi(timeStr.substr(0, colonPos));
+    int minutes = stoi(timeStr.substr(colonPos + 1));
+    return hours * 60 + minutes;
+}
