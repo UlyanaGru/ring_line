465923e2d856abb75838c5a0cc78f1e7c5fddb4e
UlyanaGru <giperbolicheskiisinys@gmail.com>
2025-09-15 07:38:41 +0300

    converting

--- ring_line.java
+++ ring_line.java
@@ -1,2 +1,13 @@
 import java.util.*;
 import java.util.stream.Collectors;
+
+public class Main {
+    //Конвертирует строку времени 'hh:mm' в минуты с 00:00
+    private static int timeToMinutes(String timeStr) {
+        String[] parts = timeStr.split(":");
+        int hours = Integer.parseInt(parts[0]);
+        int minutes = Integer.parseInt(parts[1]);
+        return hours * 60 + minutes;
+    }
+    
+    
